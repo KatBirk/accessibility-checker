d8886a6b203d59dc83ecff0ef712811e96a4b5bd
Patrick Bjerre <patrickbjerre@outlook.dk>
2025-09-04 10:49:36 +0200

    added better requirements and set the output to violations only

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ from axe_selenium_python import Axe
 
 def test_google():
     driver = webdriver.Firefox()
-    driver.get("http://www.google.com")
+    driver.get("https://www.berkshirehathaway.com/")
     axe = Axe(driver)
     # Inject axe-core javascript into page.
     axe.inject()
     # Run axe accessibility checks.
     results = axe.run()
     # Write results to file
-    axe.write_results(results, 'a11y.json')
+    axe.write_results(results["violations"], 'a11y.json')
     driver.close()
-    # Assert no violations are found
-    assert len(results["violations"]) == 0, axe.report(results["violations"])
 
 test_google()
