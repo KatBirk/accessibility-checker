bc713e7bb3e0556f7d78a61cb54ac24f6638bffd
Patrick Bjerre <patrickbjerre@outlook.dk>
2025-09-25 15:06:13 +0200

    the unfolding of nested sitemaps now works

--- sitemap.py
+++ sitemap.py
@@ -2,22 +2,57 @@ import requests
 import xml.etree.ElementTree as ET
 import re
 
+# Regex for detection of urls
+regexHttps = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)"
+
+
+def extractUrlsFromSitemapIndex(sitemap, url, recursionOverflow):
+    sitemapLinks = []
+
+    # Asuming that infinite loop with 100+ sitemaps nestings
+    if recursionOverflow >= 100:
+        return sitemapLinks
+
+    links = re.findall(regexHttps, (requests.get(sitemap).text))
+
+    for i in range(len(links)):
+        # We are asuming all .xml files are sitemaps
+        if links[i][1].__contains__(".xml"):
+            sitemapLinks.extend(
+                extractUrlsFromSitemapIndex(
+                    url + links[i][1], url, recursionOverflow + 1
+                )
+            )
+        else:
+            sitemapLinks.append(url + links[i][1])
+    return sitemapLinks
+
 
 def sitemapsFromUrl(url):
     clankerUrl = url + "robots.txt"
-    data = requests.get(clankerUrl)
-    robotData = data.text.splitlines()
-    # Regex for detection of https and http links
-    regexHttps = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)"
+    clankerData = requests.get(clankerUrl)
+    robotData = clankerData.text.splitlines()
+    sitemaps = []
+
+    # This may seem redundant, but if a website has multiple Sitemaps in robots.txt this can handle it
     for i in range(len(robotData)):
+
         if robotData[i].__contains__("Sitemap"):
             siteUrl = (robotData[i].split(" "))[1]
-            sitemaps = requests.get(siteUrl).text
-            if sitemaps.splitlines()[1].__contains__("sitemapindex"):
-                sitemapssss = re.findall(regexHttps,sitemaps)
-                print(sitemapssss)
+            sitemapsindex = requests.get(siteUrl).text
+
+            if sitemapsindex.splitlines()[1].__contains__("sitemapindex"):
+                sitemapssss = re.findall(regexHttps, sitemapsindex)
+
+                for j in range(len(sitemapssss)):
+                    # We are asuming all .xml files are sitemaps (may cause issues later)
+                    if sitemapssss[j][1].__contains__(".xml"):
+                        sitemaps.extend(
+                            extractUrlsFromSitemapIndex(url + sitemapssss[j][1], url, 0)
+                        )
+
+    # print(sitemaps) #uncomment for debugging
+    return sitemaps
 
 
-
-
-sitemapsFromUrl("https://www.google.com/")
+sitemapsFromUrl("https://www.sdu.dk/")
